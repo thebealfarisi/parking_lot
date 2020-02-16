9066985012dc8c7f3d9d0de7582552f5533c6c77
thebe <thebe.alfarisi@gmail.com>
2020-02-16 21:24:51 +0700

    Add main process

---
+++ Main.java
@@ -0,0 +1,80 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws NumberFormatException, Exception {
+		Queue<String> arguments = new LinkedList<>(Arrays.asList(args));
+		
+		try {
+			if (arguments.isEmpty()) {
+				String[] arrayData = null;
+				try {
+					while (true) {
+						String argument = "";
+						Scanner keyboardInput = new Scanner(System.in);
+						argument += keyboardInput.nextLine();
+						String[] temp = argument.split(" ");
+						processData(temp, arrayData);
+					}
+				} catch (Exception e) {
+					System.out.println("Wrong command");
+				}
+			} else {
+				String command = arguments.remove();
+				System.out.println(command);
+				try {
+					File file = new File(command); 
+					FileReader fr = new FileReader(file); 
+					BufferedReader br = new BufferedReader(fr);
+					String line;
+					String[] arrayData = null;
+					while ((line = br.readLine()) != null) {
+						String[] temp = line.split(" ");
+						processData(temp, arrayData);
+					}
+					fr.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static void processData(String[] temp, String[] arrayData) throws Exception {
+		try {
+			if (temp[0].equalsIgnoreCase("create_parking_lot")) {
+				arrayData = Process.createLot(Integer.parseInt(temp[1]));
+			} else if (temp[0].equalsIgnoreCase("park")) {
+				arrayData = Process.parkLot(arrayData, temp[1] + " " + temp[2]);
+			} else if (temp[0].equalsIgnoreCase("leave")) {
+				arrayData = Process.leaveLot(arrayData, Integer.parseInt(temp[1]));
+			} else if (temp[0].equalsIgnoreCase("status")) {
+				Process.checkLotStatus(arrayData);
+			} else if (temp[0].equalsIgnoreCase("registration_numbers_for_cars_with_colour")) {
+				Process.listRegByColor(arrayData, temp[1]);
+			} else if (temp[0].equalsIgnoreCase("slot_numbers_for_cars_with_colour")) {
+				Process.listSlotByColor(arrayData, temp[1]);
+			} else if (temp[0].equalsIgnoreCase("slot_number_for_registration_number")) {
+				Process.listRegPosition(arrayData, temp[1]);
+			} else {
+				System.out.println("Wrong command");
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Wrong command");
+		}
+	}
+
+}
---
+++ Process.java
@@ -0,0 +1,136 @@
+
+
+public class Process {
+	
+	public static String[] createLot(Integer number) throws Exception {
+		String[] result = new String[number];
+		
+		try {
+			for (int i = 0; i < number; i++) {
+				result[i] = "" + (i+1);
+			}
+			
+			System.out.println("Create parking lot with " + result.length + " slot(s)");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return result;
+	}
+	
+	public static String[] parkLot(String[] array, String data) throws Exception {
+		String[] result = array;
+		String info = "";
+		
+		for (int i = 0; i < array.length; i++) {
+			String[] temp = array[i].split(" ");
+			
+			if (temp.length < 2) {
+				String tempData = temp[0] + " " + data;
+				array[i] = tempData;
+				info = "Allocated slot number: " + (i + 1);
+				break;
+			}
+		}
+		
+		if (info.equalsIgnoreCase("")) {
+			info = "Sorry, parking lot is full";
+		}
+		
+		System.out.println(info);
+		
+		return result;
+	}
+	
+	public static String[] leaveLot(String[] array, Integer parkNumber) throws Exception {
+		String result[] = array;
+		
+		try {
+			String temp = result[parkNumber - 1];
+			temp = "" + parkNumber;
+			array[parkNumber - 1] = temp;
+			System.out.println("Leave " + parkNumber);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("There are only " + array.length + " parking lot");
+		}
+				
+		return result;
+	}
+	
+	public static void checkLotStatus(String[] array) throws Exception {
+		String dataOut = "";
+		for (String data : array) {
+			String[] temp = data.split(" ");
+			
+			if (temp.length > 1) {
+				dataOut += data + "\r\n";
+			}
+		}
+		System.out.println(dataOut);
+	}
+	
+	public static void listRegByColor(String[] array, String color) throws Exception {
+		String dataOut = "";
+		
+		for (int i = 0; i < array.length; i++) {
+			String[] temp = array[i].split(" ");
+			
+			if (temp.length > 1) {
+				if (temp[2].equalsIgnoreCase(color)) {
+					dataOut += (temp[1] + ", ");
+				}
+			}
+		}
+		
+		if (dataOut.equalsIgnoreCase("")) {
+			dataOut = "Not Found";
+		}
+		
+		System.out.println(dataOut);
+	}
+	
+	public static void listSlotByColor(String[] array, String color) throws Exception {
+		String dataOut = "";
+		
+		for (int i = 0; i < array.length; i++) {
+			String[] temp = array[i].split(" ");
+			
+			if (temp.length > 1) {
+				if (temp[2].equalsIgnoreCase(color)) {
+					dataOut += (temp[0] + ", ");
+				}
+			}
+		}
+		
+		if (dataOut.equalsIgnoreCase("")) {
+			dataOut = "Not Found";
+		}
+		
+		System.out.println(dataOut);
+	}
+	
+	public static void listRegPosition (String[] array, String regNumber) throws Exception {
+
+		String dataOut = "";
+		
+		for (int i = 0; i < array.length; i++) {
+			String[] temp = array[i].split(" ");
+			
+			if (temp.length > 1) {
+				if (temp[1].equalsIgnoreCase(regNumber)) {
+					dataOut += (temp[0] + ", ");
+				}
+			}
+		}
+		
+		if (dataOut.equalsIgnoreCase("")) {
+			dataOut = "Not Found";
+		}
+		
+		System.out.println(dataOut);
+	
+	}
+	
+
+}
